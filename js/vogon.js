fce63a1025d2dd9d0b714978d45d639ec39d4601
codexico <codexico@gmail.com>
2010-11-25 18:44:32 -0200

    organizando codigo

--- js/vogon.js
+++ js/vogon.js
@@ -1,15 +1,30 @@
 jQuery(document).ready(function ($) {
+
+  var vogon = (function () {
+    
+    var init = function () {
+      buscaSubmarino();
+    },
+    buscaSubmarino =  function () {
+      
+      $.ajax({
+        type: "GET",
+        url: "http://www.submarino.com.br/busca",
+        data: {q : "wii"},
+        success: function(html){
+          var listaDeProdutos = $(html.responseText).find('.productVitrine').text();
+          console.log(listaDeProdutos);
+          $("body").append(listaDeProdutos);
+        }
+      });
   
-  $.ajax({
-      type: "GET",
-      url: "http://www.submarino.com.br/busca",
-      data: {q : "wii"},
-      success: function(html){
-        var listaDeProdutos = $(html.responseText).find('.productVitrine').text();
-        console.log(listaDeProdutos);
-        $("body").append(listaDeProdutos);
-      }
-    });
-  
-  
+    }
+   
+    return {
+        init: init
+    };
+  }());
+
+  vogon.init();
+
 });
