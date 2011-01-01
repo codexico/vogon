5c4f43d2764fed5732abe5d0c33885d70d6defa9
codexico <codexico@gmail.com>
2011-01-01 14:05:04 -0200

    as vezes providers nao fornecem name ou email

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -5,8 +5,24 @@ class User < ActiveRecord::Base
   validates :email, :allow_blank => true, :email => true
   
   def self.create_from_hash!(hash)
-    #usar email para nome se provider nao fornecer name (yahoo envia name: " ")
-    hash['user_info']['name'] = hash['user_info']['email'] if hash['user_info']['name'].strip.empty?
+    #usar email para nome se provider nao fornecer name
+    #(yahoo envia name: " ")
+    #(openid envia user_info vazio em alguns casos)
+    if hash['user_info']['name']
+      if hash['user_info']['name'].strip.empty?
+        if hash['user_info']['email']
+          hash['user_info']['name'] = hash['user_info']['email'] 
+        else
+          hash['user_info']['name'] = hash['uid'] 
+        end
+      end
+    else
+     if hash['user_info']['email']
+        hash['user_info']['name'] = hash['user_info']['email'] 
+      else
+        hash['user_info']['name'] = hash['uid'] 
+      end
+    end
 
     create(:name => hash['user_info']['name'], :email => hash['user_info']['email'])
   end
